439b037a9374063efe86a39cd4e3b451125df453
Jesus <jesus.gaspar@externos-cl.cencosud.com>
2023-06-21 00:11:14 -0400

    :sparkles: the security config is added

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  app.use(helmet());
+  app.enableCors();
   const config = new DocumentBuilder()
     .setTitle(process.env.APP_NAME)
     .setDescription(process.env.APP_DESC)
